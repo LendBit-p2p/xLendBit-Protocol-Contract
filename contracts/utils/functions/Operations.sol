afe7a7b0b1c5a88bd9da1394166ab7e37b4a06bf
PhantomOz <faniogor@gmail.com>
2024-10-27 14:54:43 +0100

    This adds Operations

---
+++ contracts/utils/functions/Operations.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/**
+ * @title Operations
+ * @author LendBit Finance
+ *
+ * Public write-only functions that allows writing into the state of LendBit
+ */
+contract Operations {}
