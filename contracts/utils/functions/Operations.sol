b49e9e0ad0ef8dcc548b3af64173cd66dd03c0c5
PhantomOz <faniogor@gmail.com>
2024-10-28 02:18:03 +0100

    This adds creatingLendingRequest to Operations and some Utils Function

--- contracts/utils/functions/Operations.sol
+++ contracts/utils/functions/Operations.sol
@@ -5,6 +5,7 @@ import {LibAppStorage} from "../../libraries/LibAppStorage.sol";
 import {LibGettersImpl} from "../../libraries/LibGetters.sol";
 import {Validator} from "../validators/Validator.sol";
 import {Constants} from "../constants/Constant.sol";
+import {Utils} from "./Utils.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../../model/Protocol.sol";
@@ -83,6 +84,106 @@ contract Operations {
             msg.sender,
             _tokenCollateralAddress,
             _amountOfCollateral
+        );
+    }
+
+    function createLendingRequest(
+        uint128 _amount,
+        uint16 _interest,
+        uint256 _returnDate,
+        address _loanCurrency
+    ) external {
+        Validator._moreThanZero(_amount);
+
+        if (!_appStorage.s_isLoanable[_loanCurrency]) {
+            revert Protocol__TokenNotLoanable();
+        }
+
+        if ((_returnDate - block.timestamp) < 1 days) {
+            revert Protocol__DateMustBeInFuture();
+        }
+
+        uint8 decimal = LibGettersImpl._getTokenDecimal(_loanCurrency);
+
+        uint256 _loanUsdValue = LibGettersImpl._getUsdValue(
+            _appStorage,
+            _loanCurrency,
+            _amount,
+            decimal
+        );
+
+        if (_loanUsdValue < 1) revert Protocol__InvalidAmount();
+
+        uint256 collateralValueInLoanCurrency = LibGettersImpl
+            ._getAccountCollateralValue(_appStorage, msg.sender);
+
+        uint256 maxLoanableAmount = Utils.maxLoanableAmount(
+            collateralValueInLoanCurrency
+        );
+
+        if (
+            _appStorage.addressToUser[msg.sender].totalLoanCollected +
+                _loanUsdValue >=
+            maxLoanableAmount
+        ) {
+            revert Protocol__InsufficientCollateral();
+        }
+
+        address[] memory _collateralTokens = LibGettersImpl
+            ._getUserCollateralTokens(_appStorage, msg.sender);
+
+        _appStorage.requestId = _appStorage.requestId + 1;
+        Request storage _newRequest = _appStorage.request[
+            _appStorage.requestId
+        ];
+        _newRequest.requestId = _appStorage.requestId;
+        _newRequest.author = msg.sender;
+        _newRequest.amount = _amount;
+        _newRequest.interest = _interest;
+        _newRequest.returnDate = _returnDate;
+        _newRequest.totalRepayment = Utils.calculateLoanInterest(
+            _returnDate,
+            _amount,
+            _interest
+        );
+        _newRequest.loanRequestAddr = _loanCurrency;
+        _newRequest.collateralTokens = _collateralTokens;
+        _newRequest.status = Status.OPEN;
+
+        uint256 collateralToLock = Utils.calculateColateralToLock(
+            _loanUsdValue,
+            maxLoanableAmount
+        );
+
+        for (uint256 i = 0; i < _collateralTokens.length; i++) {
+            address token = _collateralTokens[i];
+            uint8 _decimalToken = LibGettersImpl._getTokenDecimal(token);
+            uint256 userBalance = _appStorage.s_addressToCollateralDeposited[
+                msg.sender
+            ][token];
+
+            // Calculate the amount to lock for each token based on its proportion of the total collateral
+            uint256 amountToLockUSD = (LibGettersImpl._getUsdValue(
+                _appStorage,
+                token,
+                userBalance,
+                _decimalToken
+            ) * collateralToLock) / 100;
+
+            uint256 amountToLock = ((((amountToLockUSD) * 10) /
+                LibGettersImpl._getUsdValue(_appStorage, token, 10, 0)) *
+                (10 ** _decimalToken)) / (Constants.PRECISION);
+
+            _appStorage.s_idToCollateralTokenAmount[_appStorage.requestId][
+                token
+            ] = amountToLock;
+        }
+
+        emit RequestCreated(
+            msg.sender,
+            _appStorage.requestId,
+            _amount,
+            _interest
         );
     }
 }
--- contracts/utils/functions/Utils.sol
+++ contracts/utils/functions/Utils.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import {Constants} from "../constants/Constant.sol";
+import "../validators/Error.sol";
+
 library Utils {
     /// @notice calculate percentage of a number
     /// @dev _percentage is in basis points (10000 = 100%, 100 = 1%, 1 = 0.01%)
@@ -12,5 +15,35 @@ library Utils {
         uint16 _percentage
     ) internal pure returns (uint256) {
         return (_number * _percentage) / 10000;
+    }
+
+    function maxLoanableAmount(
+        uint256 _collateralValueInLoanCurrency
+    ) internal pure returns (uint256 _value) {
+        _value =
+            (_collateralValueInLoanCurrency *
+                Constants.COLLATERALIZATION_RATIO) /
+            100;
+    }
+
+    function calculateLoanInterest(
+        uint256 _returnDate,
+        uint256 _amount,
+        uint16 _interest
+    ) internal view returns (uint256 _totalRepayment) {
+        if (_returnDate < block.timestamp)
+            revert Protocol__DateMustBeInFuture();
+        // Calculate the total repayment amount including interest
+        _totalRepayment = _amount + calculatePercentage(_amount, _interest);
+        return _totalRepayment;
+    }
+
+    function calculateColateralToLock(
+        uint256 _loanUsdValue,
+        uint256 _maxLoanableAmount
+    ) internal pure returns (uint256 _value) {
+        _value =
+            (_loanUsdValue * 100 * Constants.PRECISION) /
+            _maxLoanableAmount;
     }
 }
