6f5919efd479207b769d24d546f943367beffb34
PhantomOz <faniogor@gmail.com>
2024-10-29 12:35:49 +0100

    This adds encodeActionPayload and DecodeActionPayload to Message

--- contracts/utils/functions/Message.sol
+++ contracts/utils/functions/Message.sol
@@ -1,4 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract Message {}
+import {LibBytes} from "../../libraries/LibBytes.sol";
+import "../../model/Protocol.sol";
+
+contract Message {
+    using LibBytes for bytes;
+
+    function encodeActionPayload(
+        ActionPayload memory payload
+    ) internal pure returns (bytes memory) {
+        return
+            abi.encodePacked(
+                uint8(payload.action),
+                payload.sender,
+                payload.assetAddress,
+                payload.assetAmount
+            );
+    }
+
+    function decodeActionPayload(
+        bytes memory serialized
+    ) internal pure returns (ActionPayload memory params) {
+        uint256 index = 0;
+
+        params.action = Action(serialized.toUint8(index));
+        index += 1;
+
+        params.sender = serialized.toAddress(index);
+        index += 20;
+
+        params.assetAddress = serialized.toAddress(index);
+        index += 20;
+
+        params.assetAmount = serialized.toUint256(index);
+    }
+}
